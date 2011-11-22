a6faa93a020621fefe112c32bdb434b4d735cb43
Jesse White <jawhite@mail.bradley.edu>
2011-11-22 14:13:54 -0600

    xmldata

--- Resources/data/xmldata.js
+++ Resources/data/xmldata.js
@@ -1,47 +1,99 @@
-exports.xmlfoo = function (){
+var api = {};
 
 
-var data = [];
-var xhr = Titanium.Network.createHTTPClient();
-xhr.open('POST','http://ctf.playamericalive.com/form.php');
-
-xhr.onload = function(e)
-{
-  
-	var xml = this.responseXML;
-  		
-	var games = xml.documentElement.getElementsByTagName("game");
+function findGames (latitude, longitude) { 
+	var data = [];
 	
-  		
+	var xhr = Titanium.Network.createHTTPClient();
+	xhr.open('POST','http://ctf.playamericalive.com/form.php');
 	
+	xhr.send({
+		action:'findGames',
+		latitude: latitude,
+		longitude: longitude
+	});
 	
-  		
-	alert(games);	
-	for (var i = 0; i < games.length; i++) {
-	
-  		
-    	data.push({
-    		user: games.item(i).getElementsByTagName("userName").item(0).text,
-    		playerID: games.item(i).getElementsByTagName("playerID").item(0).text,
-    		
-    	});
-    	
-	}
-	//alert(data);
+	xhr.onload = function(e) {
+		//3
+		//alert('onload called');
 		
- 	
-};
-
-
-
-
-xhr.send({
-	action:'gamePlayers',
-	gameID:'1'
-});
-
-if (xhr.done){
-	alert(data);
-	return data;
+		var xml = this.responseXML;
+	  		
+		var games = xml.documentElement.getElementsByTagName("game");
+		
+			
+		for (var i = 0; i < games.length; i++) {
+			
+	    	data.push({
+	    		userName: games.item(i).getElementsByTagName("userName").item(0).text,
+	    		playerID: games.item(i).getElementsByTagName("playerID").item(0).text,
+	    	}); 	
+		}
+		
+	 	Ti.App.fireEvent('currentPlayers', {data:data});
+	}
 }
-};
+
+
+/*------------------------------------------------------------------------------------------*/
+
+
+
+
+function currentPlayers (gameID) { 
+	var data = [];
+	
+	var xhr = Titanium.Network.createHTTPClient();
+	xhr.open('POST','http://ctf.playamericalive.com/form.php');
+	
+	xhr.send({
+		action:'gamePlayers',
+		gameID:gameID
+	});
+	
+	xhr.onload = function(e) {
+		//3
+		//alert('onload called');
+		
+		var xml = this.responseXML;
+	  		
+		var games = xml.documentElement.getElementsByTagName("game");
+		
+			
+		for (var i = 0; i < games.length; i++) {
+			
+	    	data.push({
+	    		userName: games.item(i).getElementsByTagName("userName").item(0).text,
+	    		playerID: games.item(i).getElementsByTagName("playerID").item(0).text,
+	    	}); 	
+		}
+		
+	 	Ti.App.fireEvent('currentPlayers', {data:data});
+	}
+}
+
+/*------------------------------------------------------------------------------------------*/
+
+
+
+/*------------------------------------------------------------------------------------------*/
+
+
+
+
+
+
+/*------------------------------------------------------------------------------------------*/
+
+
+
+/*------------------------------------------------------------------------------------------*/
+
+// add methods
+api.currentPlayers = currentPlayers;
+api.findGames = findGames;
+
+//public interface
+exports.api = api;
+
+
